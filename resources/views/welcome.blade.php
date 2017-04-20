d649cf4b92e4e15193e642d490d680c9ddb4e893
regineblanco <regineannblanco@yahoo.com>
2017-04-20 16:53:02 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title')
+    arti
+@endsection
+
+@section('content')
+    a<strong>rt</strong>i<br>
+    a microblogging web app where you can share your random thoughts and musings, photos that you want the world to see.<br>
+    arti is a spelled-out acronym of "rt"-- random thoughts. it also has double meaning only people who speak tagalog will know.<br>
+    created by aspiring web developer regine blanco.<br>
+    <button class="btn btn-default">sign up</button>
+    <button class="btn btn-default">log in</button>
+    <button class="btn btn-default">lurk around</button>
+@endsection
