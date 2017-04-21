b7aadb68bc377e382c01b1279c0f70c3f2362a6e
regineblanco <regineannblanco@yahoo.com>
2017-04-21 16:40:10 +0800

    updated sql file

--- milestone-3.sql
+++ milestone-3.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 20, 2017 at 10:47 AM
+-- Generation Time: Apr 21, 2017 at 10:38 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -32,6 +32,54 @@ CREATE TABLE `migrations` (
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(2, '2014_10_12_000000_create_users_table', 1),
+(3, '2014_10_12_100000_create_password_resets_table', 1),
+(4, '2017_04_21_024306_create_posts_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `body` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `created_at`, `updated_at`, `body`, `user_id`) VALUES
+(2, '2017-04-20 20:19:02', '2017-04-20 20:19:02', 'hello there', 1),
+(3, '2017-04-20 20:24:28', '2017-04-20 20:24:28', 'regine blanco', 1),
+(4, '2017-04-20 23:02:33', '2017-04-20 23:02:33', 'one to three', 1),
+(9, '2017-04-21 00:04:09', '2017-04-21 00:04:09', 'simply me!', 2),
+(10, '2017-04-21 00:04:17', '2017-04-21 00:04:17', 'yeeeheee', 2),
+(11, '2017-04-21 00:05:10', '2017-04-21 00:05:10', 'yay!', 1),
+(12, '2017-04-21 00:05:19', '2017-04-21 00:05:19', 'post for the nth time', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -41,7 +89,7 @@ CREATE TABLE `migrations` (
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -53,7 +101,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Regine Blanco', 'regineannblanco@yahoo.com', '$2y$10$jELQDbZP8kGzuQvxZ35doezbSB96W0GCsZt5bhbyW.PVrJwy.ZWAO', 'F4XSNRsmr4ZHkmaXwIT4uVuVmr7007dNg4ubnKug8oG1NrtF5od02QfSJB79', '2017-04-19 20:23:18', '2017-04-19 20:23:18');
+(1, 'regine blanco', 'regineannblanco@yahoo.com', '$2y$10$imQ5anV6UnLI6OfgSe.jpOhItNz/y2x93TUCpf8/4Aq5nX4tKoAOm', '9IbnybgTnpEm9pxYGA2EqXL2nEMa17wzDLzzmUnwZipJhB5vpx8xcDBpzC77', '2017-04-20 20:15:42', '2017-04-20 20:15:42'),
+(2, 'simply me', 'simplyme@yahoo.com', '$2y$10$gvET0oU.ErsmkbB0G8uwDe4TuzBlbDnYsHXTBfGlSUcIkMBirxDFm', 'H2HFutui2zyUcJc4EtfTfo1SDg24JexDTZ0G19n1mqg7OCydBF3Av7qDeTyk', '2017-04-21 00:04:01', '2017-04-21 00:04:01');
 
 --
 -- Indexes for dumped tables
@@ -66,10 +115,23 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -79,12 +141,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
